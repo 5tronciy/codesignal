bb86c688f75f57d90a96ae76eb5d9800a825384e
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-08-12 16:54:57 +0300

    add task #19

---
+++ javascript/019-areEquallyStrong/main.js
@@ -0,0 +1,6 @@
+function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
+  return (
+    yourLeft + yourRight === friendsLeft + friendsRight &&
+    Math.abs(yourLeft - yourRight) === Math.abs(friendsLeft - friendsRight)
+  );
+}
